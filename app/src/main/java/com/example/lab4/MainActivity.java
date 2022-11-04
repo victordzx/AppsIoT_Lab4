c7ba89b350d25f5a6a0eea0b3642f93727126a16
Labtel <darwin.aponte@pucp.edu.pe>
2022-11-04 09:14:50 -0500

    Admin

--- app/src/main/java/com/example/lab4/MainActivity.java
+++ app/src/main/java/com/example/lab4/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.lab4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("IoT LAB 4");
+
+    }
+
+    public void irAdmin(View view){
+        Intent intent = new Intent(MainActivity.this,MainActivityAdmin.class);
+        startActivity(intent);
+
     }
 }
---
+++ app/src/main/java/com/example/lab4/MainActivityAdmin.java
@@ -0,0 +1,15 @@
+package com.example.lab4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivityAdmin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_admin);
+        setTitle("IoT LAB 4");
+    }
+}

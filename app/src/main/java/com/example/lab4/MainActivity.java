f5e0154e17626ee247dca44f36428a3c83713c8f
Joau0824 <a20182162@pucp.edu.pe>
2022-11-06 20:27:01 -0500

    autenticacion

--- app/src/main/java/com/example/lab4/MainActivity.java
+++ app/src/main/java/com/example/lab4/MainActivity.java
@@ -1,37 +1,83 @@
 package com.example.lab4;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityOptionsCompat;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
 
-
+    FirebaseDatabase firebaseDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        firebaseDatabase = FirebaseDatabase.getInstance();
+
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.txt_layaout);
 
+        Button btnLogin = findViewById(R.id.Admin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent fbIntent = AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(Arrays.asList(
+                                new AuthUI.IdpConfig.EmailBuilder().build(),
+                                new AuthUI.IdpConfig.GoogleBuilder().build()
+                        ))
+                        .build();
+                signInLauncher.launch(fbIntent);
+
+            }
+        });
+
     }
 
-    public void irAdmin(View view) {
-        Intent intent = new Intent(MainActivity.this, MainActivityAdmin.class);
-        startActivity(intent);
+    private ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
+            new FirebaseAuthUIActivityResultContract(), result -> {
+                onSignInOnResult(result);
+
+            });
+    private void onSignInOnResult(FirebaseAuthUIAuthenticationResult result){
+        IdpResponse idpResponse = result.getIdpResponse();
+        if(result.getResultCode() == RESULT_OK){
+            Log.d("msg-fb",idpResponse.getEmail());
+        }else{
+            Log.d("msg-fb","Error al loguearse");
+        }
+
+
     }
 
     public void irUser(View view) {
         Intent intent = new Intent(MainActivity.this, UserActivity.class);
         startActivity(intent);
     }
+
+
 }
